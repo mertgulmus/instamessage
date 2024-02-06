0f0b275871788516d9456868a75c87258e29e0fc
Mert Gulmus <mertgulms@protonmail.com>
2024-02-06 12:44:52 +0200

    macos working version

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+/**
+ * @fileoverview This is the main server file for the InstaMessage backend.
+ * It sets up the Express server, connects to the database, and defines the routes.
+ * @module server
+ */
+
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -21,12 +27,23 @@ app.use('/api/user/', usersRouter);
 app.use('/api/message/', messagesRouter);
 app.use('/api/chat/', chatsRouter);
 
-// routes
+/**
+ * Default route that returns a response when the root URL is accessed.
+ * @name GET /
+ * @function
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
 app.get('/', (req, res) => {});
 
 // database
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
+        /**
+         * Callback function that is called when the database connection is successful.
+         * It starts the server and listens for incoming requests.
+         * @callback listenCallback
+         */
         app.listen(process.env.PORT, () => console.log('Connected to the database and listening for port', process.env.PORT));
     })
     .catch(err => console.log(err));
